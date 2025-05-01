bd36b309b3dc90d2fbfef83a0ed0a2b4c50879b8
RJ Salvador <salvador.workshop@gmail.com>
2025-05-01 09:57:05 -0400

    Refining candle holder model

--- apps/jscad-web/src/projects.js
+++ apps/jscad-web/src/projects.js
@@ -5,5 +5,6 @@ export const swProjects = [
   { name: 'SW Builders (Columns)', source: './projects/column-examples.js' },
   { name: 'SW Builders (N-Foils)', source: './projects/foil-examples.js' },
   { name: 'SW Builders (Moulds)', source: './projects/mould-examples.js' },
-  { name: 'Candle Holder 01', source: './projects/candle-holder-01.js' },
+  { name: 'Candle Holder v1', source: './projects/candle-holder/candle-holder-01.js' },
+  { name: 'Candle Holder v2', source: './projects/candle-holder/index.js' },
 ]
