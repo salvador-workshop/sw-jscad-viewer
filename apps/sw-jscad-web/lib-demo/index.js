0fa3fc32250bced119755d5a326f46a1df580f86
RJ Salvador <salvador.workshop@gmail.com>
2026-04-01 19:37:55 -0400

    Reworking base demo

--- apps/sw-jscad-web/lib-demo/index.js
+++ apps/sw-jscad-web/lib-demo/index.js
@@ -1,29 +1,32 @@
 "use strict"
 const jscad = require('@jscad/modeling')
 
-// const swJscad = require('sw-jscad').init({ lib: jscad });
-// const swjUi = require('sw-jscad-ui').init({ lib: jscad, swLib: swJscad });
-// const swjFamilies = require('sw-jscad-families').init({ lib: jscad, swLib: swJscad });
-// const swjBuilders = require('sw-jscad-builders').init({ lib: jscad, swLib: swJscad, swFamilies: swjFamilies });
-
 const swCadJs = require('swcad-js').init({ lib: jscad });
 console.log('swCadJs', swCadJs)
 
 const {
-    arches,
-    columns,
-    walls,
+    moulding,
+} = swCadJs.components
+
+const {
+    arch,
+    column,
+    wall,
+    foil: foil3d,
 } = swCadJs.models
 
-// const { profiles, prefab } = swCadJs.models
+const {
+    shapes,
+    foil: foil2d,
+} = swCadJs.profiles
 
 const {
     aranea,
 } = swCadJs.profiles.trim
 
-// const {
-//     layout,
-// } = swCadJs.models
+const {
+    layout,
+} = swCadJs.utils
 
 
 const main = () => {
@@ -36,8 +39,8 @@ const main = () => {
     //-----------
     // Profiles
 
-    const profile1 = profiles.sqCornerCircNotch({ sqLength: 5 });
-    const profile3 = profiles.octagonal({ sqLength: 5 });
+    const profile1 = shapes.square.sqCornerCircNotch({ sqLength: 5 });
+    const profile3 = shapes.octagon({ sqLength: 5 });
 
     const tFamilyBasic = aranea.buildTrimFamily({ unitHeight: 20, unitDepth: 10 });
     const dadoTrim = [
@@ -54,30 +57,30 @@ const main = () => {
     //-----------
     // Mouldings
 
-    const mould3 = prefab.mouldings.circularMoulding({ radius: 20, height: 5 }, profile1);
+    const mould3 = moulding.circularMoulding({ radius: 20, height: 5 }, profile1);
     layout.addToLayout({ name: 'mould3', desc: '...', layoutOpts }, mould3);
 
 
     //-----------
     // Foils
 
-    const foil1 = profiles.foils2d.trefoil({ radius: 10 });
+    const foil1 = foil2d.trefoil({ radius: 10 });
     layout.addToLayout({ name: 'foil1', desc: '...', layoutOpts }, foil1);
 
-    const foil5 = profiles.foils2d.quatrefoil({ radius: 10, lobeRadiusType: 'inSlice' });
+    const foil5 = foil2d.quatrefoil({ radius: 10, lobeRadiusType: 'inSlice' });
     layout.addToLayout({ name: 'foil5', desc: '...', layoutOpts }, foil5);
 
-    const foil7 = prefab.foils3d.trefoil({ radius: 15, lobeRadiusType: 'halfRadius', cutCentre: true }, profile1);
+    const foil7 = foil3d.trefoil({ radius: 15, lobeRadiusType: 'halfRadius', cutCentre: true }, profile1);
     layout.addToLayout({ name: 'foil7', desc: '...', layoutOpts }, foil7);
 
-    const foil8 = prefab.foils3d.quatrefoil({ radius: 15, lobeRadiusType: 'mean', cutCentre: true }, profile3);
+    const foil8 = foil3d.quatrefoil({ radius: 15, lobeRadiusType: 'mean', cutCentre: true }, profile3);
     layout.addToLayout({ name: 'foil8', desc: '...', layoutOpts }, foil8);
 
 
     //-----------
     // Columns
 
-    const col2 = columns.threePtColumn({
+    const col2 = column.threePtColumn({
         base: ['roundCylinder', 2, 3.5],
         shaft: ['extrude', null, profile1],
         capital: ['roundCylinder', 2, 3.5],
@@ -89,17 +92,17 @@ const main = () => {
     //-----------
     // Arches
 
-    const arch1 = arches.twoPtArch({ arcRadius: 30, archWidth: 35, profileWidth: 5 }, profile1);
+    const arch1 = arch.twoPtArch({ arcRadius: 30, archWidth: 35, profileWidth: 5 }, profile1);
     layout.addToLayout({ name: 'arch1', desc: '...', layoutOpts }, arch1);
 
-    const arch2 = arches.twoPtArch({ arcRadius: 30, archWidth: 35 });
+    const arch2 = arch.twoPtArch({ arcRadius: 30, archWidth: 35 });
     layout.addToLayout({ name: 'arch2', desc: '...', layoutOpts }, arch2);
 
 
     //-----------
     // Walls
 
-    const wall2 = walls.buildWall({
+    const wall2 = wall.buildWall({
         height: 100,
         thickness: 10,
         length: 80,
@@ -113,7 +116,7 @@ const main = () => {
     });
     layout.addToLayout({ name: 'Wall (2)', desc: '...', layoutOpts }, wall2);
 
-    const wallDado1 = walls.buildWall({
+    const wallDado1 = wall.buildWall({
         height: 100,
         thickness: 10,
         length: 70,
