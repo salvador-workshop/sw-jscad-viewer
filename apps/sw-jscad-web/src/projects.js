26d5d87606bbb6b18afc20539ad381b30f4286a5
RJ Salvador <salvador.workshop@gmail.com>
2026-04-10 10:52:33 -0400

    Demo cleanup

--- apps/sw-jscad-web/src/projects.js
+++ apps/sw-jscad-web/src/projects.js
@@ -4,57 +4,11 @@ export const baseDemos = [
   { name: 'sw-jscad (Legacy libs)', source: './lib-demo/sw-jscad.example.js' },
 ]
 
-// export const swLibCore = [
-//   { name: 'Area Points', source: './lib-demo/area-point-examples.js' },
-//   { name: 'Centroid', source: './lib-demo/centroid-examples.js' },
-//   { name: 'Demo Pack', source: './lib-demo/index.js' },
-//   { name: 'Control Points', source: './lib-demo/control-points-examples.js' },
-//   { name: 'Mesh Cuboid', source: './lib-demo/mesh-cuboid-examples.js' },
-//   { name: 'Mesh Cylinder', source: './lib-demo/mesh-cylinder-examples.js' },
-//   { name: 'Mesh Panel', source: './lib-demo/mesh-panel-examples.js' },
-//   { name: 'Rectangular Frame', source: './lib-demo/rectangular-frame-examples.js' },
-//   { name: 'Transforms', source: './lib-demo/transform-examples.js' },
-// ]
-
-// export const swBuilders = [
-//   { name: 'Arches', source: './lib-demo/builders/arch-examples.js' },
-//   { name: 'Buttress', source: './lib-demo/builders/buttress-examples.js' },
-//   { name: 'Columns', source: './lib-demo/builders/column-examples.js' },
-//   { name: 'Entryways', source: './lib-demo/builders/entryway-examples.js' },
-//   { name: 'Roofs', source: './lib-demo/builders/roof-examples.js' },
-//   { name: 'Walls', source: './lib-demo/builders/wall-examples.js' },
-// ]
-
-// export const swDetails = [
-//   { name: 'Foils', source: './lib-demo/details/foil-examples.js' },
-//   { name: 'Moulds', source: './lib-demo/details/mould-examples.js' },
-//   { name: 'Profiles', source: './lib-demo/details/profile-examples.js' },
-//   { name: 'Profiles (advanced)', source: './lib-demo/details/profile-advanced-examples.js' },
-// ]
-
-// export const swFamilies = [
-//   { name: 'Dowel Couplers', source: './lib-demo/families/dowel-coupler-examples.js' },
-//   { name: 'Dowel Jigs', source: './lib-demo/families/dowel-jig-examples.js' },
-//   { name: 'Dowel Joists', source: './lib-demo/families/dowel-joist-examples.js' },
-//   { name: 'Standard Masonry', source: './lib-demo/families/standard-masonry-examples.js' },
-//   { name: 'Standard Lumber', source: './lib-demo/families/standard-lumber-examples.js' },
-//   { name: 'Standard Tile', source: './lib-demo/families/standard-tile-examples.js' },
-//   { name: 'Standard Crafts', source: './lib-demo/families/standard-crafts-examples.js' },
-//   { name: 'Standard Paper', source: './lib-demo/families/standard-paper-examples.js' },
-//   { name: 'Trim family: Aranea', source: './lib-demo/families/trim-family-examples.js' },
-// ]
-
-// export const swUx = [
-//   { name: 'Layout frames', source: './lib-demo/layout-examples.js' },
-// ]
-
-////////
-
 export const swcadJsProfiles = [
   { name: 'Basic Profiles', source: './lib-demo/details/profile-examples.js' },
   { name: 'Connections', source: './lib-demo/profiles/connections.js' },
   { name: 'Reinforcements', source: './lib-demo/profiles/reinforcement.js' },
-  { name: 'Mesh (2D)', source: './lib-demo/profiles/mesh-2d.js' },
+  { name: 'Mesh Panel (2D)', source: './lib-demo/profiles/mesh-2d.js' },
   { name: 'Lumber', source: './lib-demo/families/standard-lumber-examples.js' },
   { name: 'Paper', source: './lib-demo/families/standard-paper-examples.js' },
   { name: 'Rectangular Frame', source: './lib-demo/rectangular-frame-examples.js' },
@@ -64,12 +18,7 @@ export const swcadJsProfiles = [
 export const swcadJsComponents = [
   { name: 'Open Web Joist', source: './lib-demo/components/open-web-joist.js' },
   { name: 'Beads and Bits', source: './lib-demo/components/beads-bits.js' },
-  { name: 'Mesh Cuboid', source: './lib-demo/mesh-cuboid-examples.js' },
-  { name: 'Mesh Cylinder', source: './lib-demo/mesh-cylinder-examples.js' },
-  { name: 'Mesh Panel', source: './lib-demo/mesh-panel-examples.js' },
-  { name: 'Dowel Couplers', source: './lib-demo/families/dowel-coupler-examples.js' },
-  { name: 'Dowel Jigs', source: './lib-demo/families/dowel-jig-examples.js' },
-  { name: 'Dowel Joists', source: './lib-demo/families/dowel-joist-examples.js' },
+  { name: 'Mesh Panel (3D)', source: './lib-demo/mesh-panel-examples.js' },
   { name: 'Moulds', source: './lib-demo/details/mould-examples.js' },
   { name: 'Brick', source: './lib-demo/families/standard-masonry-examples.js' },
   { name: 'Tile', source: './lib-demo/families/standard-tile-examples.js' },
@@ -78,11 +27,11 @@ export const swcadJsComponents = [
 export const swcadJsModels = [
   { name: 'Foil', source: './lib-demo/details/foil-examples.js' },
   { name: 'Arch', source: './lib-demo/builders/arch-examples.js' },
-  { name: 'Buttress', source: './lib-demo/builders/buttress-examples.js' },
-  { name: 'Column', source: './lib-demo/builders/column-examples.js' },
   { name: 'Wall', source: './lib-demo/builders/wall-examples.js' },
   { name: 'Wall Entryway', source: './lib-demo/builders/entryway-examples.js' },
+  { name: 'Column', source: './lib-demo/builders/column-examples.js' },
   { name: 'Roof', source: './lib-demo/builders/roof-examples.js' },
+  { name: 'Buttress', source: './lib-demo/builders/buttress-examples.js' },
 ]
 
 export const swcadJsUtils = [
