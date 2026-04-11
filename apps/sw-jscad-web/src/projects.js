f400b18594631461c667e523e014c051492dead1
RJ Salvador <salvador.workshop@gmail.com>
2026-04-10 21:41:51 -0400

    Completed update

--- apps/sw-jscad-web/src/projects.js
+++ apps/sw-jscad-web/src/projects.js
@@ -4,21 +4,34 @@ export const baseDemos = [
   { name: 'sw-jscad (Legacy libs)', source: './lib-demo/sw-jscad.example.js' },
 ]
 
+export const swcadJsCalcs = [
+  { name: 'Area Points', source: './lib-demo/area-point-examples.js' },
+  { name: 'Centroid', source: './lib-demo/centroid-examples.js' },
+  { name: 'Control Points', source: './lib-demo/control-points-examples.js' },
+  { name: 'Transforms', source: './lib-demo/transform-examples.js' },
+]
+
 export const swcadJsProfiles = [
   { name: 'Basic Profiles', source: './lib-demo/details/profile-examples.js' },
   { name: 'Connections', source: './lib-demo/profiles/connections.js' },
   { name: 'Reinforcements', source: './lib-demo/profiles/reinforcement.js' },
   { name: 'Mesh Panel (2D)', source: './lib-demo/profiles/mesh-2d.js' },
-  { name: 'Lumber', source: './lib-demo/families/standard-lumber-examples.js' },
-  { name: 'Paper', source: './lib-demo/families/standard-paper-examples.js' },
   { name: 'Rectangular Frame', source: './lib-demo/rectangular-frame-examples.js' },
   { name: 'Trim family: Aranea', source: './lib-demo/families/trim-family-examples.js' },
+]
+
+export const swcadJsProfileSpec = [
+  { name: 'Lumber', source: './lib-demo/families/standard-lumber-examples.js' },
+  { name: 'Paper', source: './lib-demo/families/standard-paper-examples.js' },
 ]
 
 export const swcadJsComponents = [
   { name: 'Open Web Joist', source: './lib-demo/components/open-web-joist.js' },
   { name: 'Beads and Bits', source: './lib-demo/components/beads-bits.js' },
   { name: 'Mesh Panel (3D)', source: './lib-demo/mesh-panel-examples.js' },
+]
+
+export const swcadJsComponentSpec = [
   { name: 'Brick', source: './lib-demo/families/standard-masonry-examples.js' },
   { name: 'Tile', source: './lib-demo/families/standard-tile-examples.js' },
 ]
@@ -31,8 +44,5 @@ export const swcadJsModels = [
 ]
 
 export const swcadJsUtils = [
-  { name: 'Area Points', source: './lib-demo/area-point-examples.js' },
-  { name: 'Centroid', source: './lib-demo/centroid-examples.js' },
-  { name: 'Control Points', source: './lib-demo/control-points-examples.js' },
-  { name: 'Transforms', source: './lib-demo/transform-examples.js' },
+
 ]
