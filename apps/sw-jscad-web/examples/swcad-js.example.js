a401d90e9fbe124a5f55ac51b1b27ff5e9837d47
RJ Salvador <salvador.workshop@gmail.com>
2026-04-11 22:26:53 -0400

    Reworking default page

--- apps/sw-jscad-web/examples/swcad-js.example.js
+++ apps/sw-jscad-web/examples/swcad-js.example.js
@@ -1,9 +1,7 @@
 "use strict"
 const jscad = require('@jscad/modeling')
 
-const { intersect, subtract } = jscad.booleans
-const { colorize } = jscad.colors
-const { cube, sphere } = jscad.primitives
+const { cube, cuboid, sphere } = jscad.primitives
 const { translate } = jscad.transforms
 
 const swcadJs = require('swcad-js').init({ jscad });
@@ -11,6 +9,7 @@ console.log('swcadJs', swcadJs)
 
 const {
   math,
+  transform,
 } = swcadJs.calcs
 
 const {
@@ -21,51 +20,84 @@ const {
   openWebJoist,
 } = swcadJs.components
 
+const {
+  routedCuboid,
+} = swcadJs.components.routedShapes
+
+const baseOpts = {
+  topBit: 'chamfer',
+  topBitOpts: {
+    radius1: 6,
+    radius2: 8,
+    offset1: 3,
+    offset2: 2,
+    offset3: 3,
+    offset4: 2,
+  },
+  bottomBit: 'none',
+  bottomBitOpts: {
+    radius1: 6,
+    radius2: 8,
+    offset1: 3,
+    offset2: 2,
+    offset3: 3,
+    offset4: 2,
+  },
+}
+const baseData = routedCuboid(baseOpts)
+const base = baseData[0]
+
+const mid = cuboid({ size: [10, 10, 10] })
+
+const topOpts = {
+  topBit: 'roundOver',
+  topBitOpts: {
+    radius1: 6,
+    radius2: 8,
+    offset1: 3,
+    offset2: 2,
+    offset3: 3,
+    offset4: 2,
+  },
+  bottomBit: 'cove',
+  bottomBitOpts: {
+    radius1: 6,
+    radius2: 8,
+    offset1: 3,
+    offset2: 2,
+    offset3: 3,
+    offset4: 2,
+  },
+}
+const topData = routedCuboid(topOpts)
+const top = topData[0]
+
+const introPlinth = transform.stack({}, [base, mid, top])
+
+const openWebJoistOpts1 = {
+  length: math.inchesToMm(6),
+  width: math.inchesToMm(1.25),
+}
+
+const openWebJoistOpts2 = {
+  length: math.inchesToMm(6.5),
+  width: math.inchesToMm(1.5),
+  reinforcementLevel: 2
+}
+
+const openWebJoistData1 = openWebJoist(openWebJoistOpts1)
+const openWebJoistData2 = openWebJoist(openWebJoistOpts2)
+
+const openWebJoistModel1 = openWebJoistData1[0]
+const openWebJoistModel2 = openWebJoistData2[0]
+
 function main() {
-  const outer = subtract(
-    cube({ size: 10 }),
-    sphere({ radius: 6.8 })
-  )
-
-  const inner = intersect(
-    sphere({ radius: 4 }),
-    cube({ size: 7 })
-  )
-
-  const spaceUnit = math.inchesToMm(2.25)
-
-  const openWebJoistOpts1 = {
-    length: math.inchesToMm(6),
-    width: math.inchesToMm(1.25),
-  }
-
-  const openWebJoistOpts2 = {
-    length: math.inchesToMm(6.5),
-    width: math.inchesToMm(1.5),
-    reinforcementLevel: 2
-  }
-
-  const openWebJoistOpts3 = {
-    length: math.inchesToMm(7),
-    width: math.inchesToMm(1.75),
-    reinforcementLevel: 3
-  }
-
-  const openWebJoistData1 = openWebJoist(openWebJoistOpts1)
-  const openWebJoistData2 = openWebJoist(openWebJoistOpts2)
-  const openWebJoistData3 = openWebJoist(openWebJoistOpts3)
-
-  const openWebJoistModel1 = openWebJoistData1[0]
-  const openWebJoistModel2 = openWebJoistData2[0]
-  const openWebJoistModel3 = openWebJoistData3[0]
+  const spaceUnit = math.inchesToMm(3.5)
 
   return [
-    translate([spaceUnit * 0, spaceUnit * 0, spaceUnit * 0], colorize(colors.lightGreen, outer)),
-    translate([spaceUnit * 0, spaceUnit * 0, spaceUnit * 0], colorize(colors.orange, inner)),
-
+    translate([spaceUnit * 0, spaceUnit * 0, spaceUnit * 0], introPlinth),
     translate([spaceUnit * 1, spaceUnit * 0, spaceUnit * 0], openWebJoistModel1),
     translate([spaceUnit * 2, spaceUnit * 0, spaceUnit * 0], openWebJoistModel2),
-    translate([spaceUnit * 3, spaceUnit * 0, spaceUnit * 0], openWebJoistModel3),
   ]
 }
 
